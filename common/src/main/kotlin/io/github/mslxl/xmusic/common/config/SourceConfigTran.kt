761abb55dc064e561e1d4567cc0a51d17ba79889
Mslxl <i@mslxl.com>
2022-01-06 17:59:12 +0800

    fix(common): config transaction got the wrong data

--- common/src/main/kotlin/io/github/mslxl/xmusic/common/config/SourceConfigTran.kt
+++ common/src/main/kotlin/io/github/mslxl/xmusic/common/config/SourceConfigTran.kt
@@ -2,8 +2,8 @@ package io.github.mslxl.xmusic.common.config
 
 import java.util.*
 
-class SourceConfigTran internal constructor(val before: SourceConfig) : SourceConfig(before.fs, before.id) {
-    override var properties: Properties = Properties()
+class SourceConfigTran internal constructor(private val before: SourceConfig) : SourceConfig(before.fs, before.id) {
+    override var properties: Properties = before.properties.clone() as Properties
     fun commit() {
         before.properties = this.properties
         before.save()
