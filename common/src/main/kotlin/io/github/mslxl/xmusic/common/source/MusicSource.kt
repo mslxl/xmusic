96c33821ee52fd62eff647b8563b9ed0b90b2bc8
Mslxl <i@mslxl.com>
2022-01-08 20:18:09 +0800

    fix(common): fix missing method

--- common/src/main/kotlin/io/github/mslxl/xmusic/common/source/MusicSource.kt
+++ common/src/main/kotlin/io/github/mslxl/xmusic/common/source/MusicSource.kt
@@ -3,10 +3,7 @@ package io.github.mslxl.xmusic.common.source
 import io.github.mslxl.xmusic.common.XMusic
 import io.github.mslxl.xmusic.common.config.SourceConfig
 import io.github.mslxl.xmusic.common.i18n.I18nKey
-import io.github.mslxl.xmusic.common.source.processor.AlbumProcessor
-import io.github.mslxl.xmusic.common.source.processor.ExplorerProcessor
-import io.github.mslxl.xmusic.common.source.processor.SearchProcessor
-import io.github.mslxl.xmusic.common.source.processor.SongProcessor
+import io.github.mslxl.xmusic.common.source.processor.*
 
 /**
  * Provide information of song from different source
@@ -33,6 +30,9 @@ interface MusicSource {
         get() = null
 
     val album: AlbumProcessor?
+        get() = null
+
+    val collection: CollectionProcessor?
         get() = null
 
     fun acceptConfig(config: SourceConfig)
