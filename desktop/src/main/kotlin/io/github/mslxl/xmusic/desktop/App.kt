cc4fe6677b34ec8dc7bbf37f6e5b8151eac20d87
Mslxl <i@mslxl.com>
2022-01-18 23:27:50 +0800

    feat(desktop): add sqlite as cache index manager database

--- desktop/src/main/kotlin/io/github/mslxl/xmusic/desktop/App.kt
+++ desktop/src/main/kotlin/io/github/mslxl/xmusic/desktop/App.kt
@@ -10,6 +10,7 @@ import io.github.mslxl.ktswing.layout.cardLayout
 import io.github.mslxl.ktswing.resizable
 import io.github.mslxl.xmusic.common.XMusic
 import io.github.mslxl.xmusic.common.logger
+import io.github.mslxl.xmusic.desktop.fs.CacheIndexDBImpl
 import io.github.mslxl.xmusic.desktop.fs.LocalFile
 import io.github.mslxl.xmusic.desktop.player.VlcjControl
 import io.github.mslxl.xmusic.desktop.src.SourceLocalMusic
@@ -24,7 +25,7 @@ object App {
     const val version = "0.0.1-alpha"
     private val logger = App::class.logger
 
-    val core: XMusic = XMusic(LocalFile(), VlcjControl).apply {
+    val core: XMusic = XMusic(LocalFile(), VlcjControl, CacheIndexDBImpl()).apply {
         addMusicSource(SourceLocalMusic())
     }
 
---
+++ desktop/src/main/kotlin/io/github/mslxl/xmusic/desktop/fs/CacheIndexDBImpl.kt
@@ -0,0 +1,80 @@
+package io.github.mslxl.xmusic.desktop.fs
+
+import io.github.mslxl.xmusic.common.fs.CacheIndexDBManager
+import io.github.mslxl.xmusic.common.logger
+import io.github.mslxl.xmusic.desktop.EnvVar
+import org.apache.commons.codec.digest.DigestUtils
+import java.io.File
+import java.sql.DriverManager
+
+class CacheIndexDBImpl : CacheIndexDBManager {
+    companion object {
+        private val logger = CacheIndexDBImpl::class.logger
+
+        private val cacheDir = File(EnvVar.programCfgDir, "cache").apply {
+            if (!exists()) mkdirs()
+        }
+        private val indexFile = File(cacheDir, "index.db")
+        private val connection = DriverManager.getConnection("jdbc:sqlite:${indexFile.absolutePath}")
+    }
+
+
+    init {
+        logger.info("init cache database")
+        connection.createStatement().use {
+            it.execute("create table if not exists cache (hash integer, path string) ")
+        }
+    }
+
+    override fun get(key: String): File? {
+        val md5 = try {
+            connection.createStatement().use {
+                val resultSet = it.executeQuery("select * from cache where hash=${key.hashCode()}")
+                resultSet.getString("path")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+        val f = File(cacheDir, md5)
+        return if (f.exists()) f else null
+    }
+
+    private fun insert(key: String, baseName: String) {
+        connection.createStatement().use {
+            it.execute("insert into cache values(${key.hashCode()}, '$baseName')")
+        }
+    }
+
+
+    override fun put(key: String, file: File) {
+        val md5 = file.inputStream().use {
+            DigestUtils.md5Hex(it)
+        }
+        val dst = File(cacheDir, md5)
+        if (!dst.exists()) {
+            file.copyTo(dst)
+        }
+        insert(key, md5)
+    }
+
+    override fun createFile(key: String, writer: (File) -> Unit): File {
+        val tmpFile = File(cacheDir, "${key.hashCode().toString(16)}.tmp")
+        try {
+            writer.invoke(tmpFile)
+        } catch (e: Exception) {
+            tmpFile.delete()
+            throw e
+        }
+        val md5 = tmpFile.inputStream().use {
+            DigestUtils.md5Hex(it)
+        }
+        val dst = File(cacheDir, md5)
+        if (!dst.exists()) {
+            tmpFile.renameTo(dst)
+        }
+        insert(key, md5)
+        return dst
+    }
+
+}
